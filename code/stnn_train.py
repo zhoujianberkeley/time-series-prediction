4580961743c4905d8ee3ba16edc721c4fdc82db6
沈廷威 <526588851@qq.com>
2021-03-21 14:40:27 +0800

    validation score差不多可以稳定在5分

--- code/stnn_train.py
+++ code/stnn_train.py
@@ -9,6 +9,42 @@ import random
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
 from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+
+loss_tracker = []
+valid_score = []
+train_score = []
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as func
+
+def RMSE(y_true, y_pred):
+    return torch.sqrt(torch.mean(torch.square(y_true - y_pred)))
+
+class ScoreLoss(nn.Module):
+
+    def forward(self, y_preds, y_true):
+        accskill_score = 0
+        rmse_scores = 0
+        a = [1.5] * 4 + [2] * 7 + [3] * 7 + [4] * 6
+        y_true_mean = torch.mean(y_true, dim=0)
+        y_pred_mean = torch.mean(y_preds, dim=0)
+        #     print(y_true_mean.shape, y_pred_mean.shape)
+
+        for i in range(24):
+            fenzi = torch.sum((y_true[:, i] - y_true_mean[i]) * (y_preds[:, i] - y_pred_mean[i]))
+            fenmu = torch.sqrt(torch.sum((y_true[:, i] - y_true_mean[i]) ** 2) * torch.sum(
+                (y_preds[:, i] - y_pred_mean[i]) ** 2))
+            cor_i = fenzi / fenmu
+
+            accskill_score += a[i] * np.log(i + 1) * cor_i
+            rmse_score = RMSE(y_true[:, i], y_preds[:, i])
+            #         print(cor_i,  2 / 3.0 * a[i] * np.log(i+1) * cor_i - rmse_score)
+            rmse_scores += rmse_score
+
+        return -(2 / 3.0 * accskill_score - rmse_scores)
 
 
 # python ref/stnn_train.py
@@ -21,8 +57,8 @@ def set_seed(seed=427):
 
 def load_data2():
     # CMIP data
-    train = xr.open_dataset('../data/enso_round1_train_20210201/CMIP_train.nc')
-    label = xr.open_dataset('../data/enso_round1_train_20210201/CMIP_label.nc')
+    train = xr.open_dataset('D:/chromeFiles/enso_round1_train_20210201/CMIP_train.nc')
+    label = xr.open_dataset('D:/chromeFiles/enso_round1_train_20210201/CMIP_label.nc')
 
     train_sst = train['sst'][:, :12].values  # (4645, 12, 24, 72)
     train_t300 = train['t300'][:, :12].values
@@ -36,8 +72,8 @@ def load_data2():
     train_sst = np.nan_to_num(train_sst)
 
     # SODA data
-    train2 = xr.open_dataset('../data/enso_round1_train_20210201/SODA_train.nc')
-    label2 = xr.open_dataset('../data/enso_round1_train_20210201/SODA_label.nc')
+    train2 = xr.open_dataset('D:/chromeFiles/enso_round1_train_20210201/SODA_train.nc')
+    label2 = xr.open_dataset('D:/chromeFiles/enso_round1_train_20210201/SODA_label.nc')
 
     train_sst2 = train2['sst'][:, :12].values  # (4645, 12, 24, 72)
     train_t3002 = train2['t300'][:, :12].values
@@ -147,10 +183,12 @@ def eval_score(preds, label):
 
 
 fit_params = {
-    # 'n_epochs': 22,
-    'n_epochs': 2,
+    'n_epochs': 15,
+    # 'n_epochs': 2,
     'learning_rate': 8e-5,
     'batch_size': 64,
+    'loss':nn.MSELoss(),
+    # 'loss':ScoreLoss()
 }
 
 
@@ -161,10 +199,12 @@ def train():
     valid_loader = DataLoader(valid_dataset, batch_size=fit_params['batch_size'])
 
     model = simpleSpatialTimeNN()
+    # device = 'cpu'
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=fit_params['learning_rate'])
-    loss_fn = nn.MSELoss()
-
+    loss_fn = fit_params['loss']
+    # loss_fn = ScoreLoss()
     model.to(device)
     loss_fn.to(device)
 
@@ -184,6 +224,7 @@ def train():
 
             preds = model(sst, t300, ua, va)
             loss = loss_fn(preds, label)
+            score = eval_score(label.cpu().detach().numpy(), preds.cpu().detach().numpy())
             loss.backward()
             # for shentingwei
             # loss.backward() computes dloss/dx for every parameter x which has requires_grad=True.
@@ -192,6 +233,9 @@ def train():
             optimizer.step()
             # optimizer.step updates the value of x using the gradient x.grad. For example, the SGD optimizer performs:
             # x += -lr * x.grad
+            loss_tracker.append(loss)
+            train_score.append(score)
+            print('Step: {}, Train Loss(score from the coder): {}'.format(step, score))
             print('Step: {}, Train Loss: {}'.format(step, loss))
 
         model.eval()
@@ -210,6 +254,8 @@ def train():
         y_true = torch.cat(y_true, axis=0)
         y_pred = torch.cat(y_pred, axis=0)
         sco = eval_score(y_true.cpu().detach().numpy(), y_pred.cpu().detach().numpy())
+        for j in range(len(train_loader)):
+            valid_score.append(sco)
         print('Epoch: {}, Valid Score {}'.format(i + 1, sco))
 
     torch.save(model.state_dict(), '../user_data/stnn.pt')
@@ -220,3 +266,17 @@ def train():
 
 if __name__ == "__main__":
     train()
+    plt.figure(1)
+    ax = plt.subplot(311)
+    ax.set_title('train_loss')
+    plt.plot(loss_tracker)
+    # plt.show()
+    ax1 = plt.subplot(312)
+    ax1.set_title('valid_score')
+    plt.plot(valid_score)
+    # plt.show()
+    ax2 = plt.subplot(313)
+    ax2.set_title('train_score')
+    plt.plot(train_score)
+    plt.show()
+    print("程序运行完毕")
